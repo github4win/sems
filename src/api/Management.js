89a72e1ffeba78e415e19e30b8b9e9e95b64cd43
github4win <52733132+github4win@users.noreply.github.com>
2020-07-24 10:55:54 +0900

    Update Management.js

--- src/api/Management.js
+++ src/api/Management.js
@@ -1,25 +1,27 @@
 import request from '@/utils/request'
 
-//메뉴 목록 조회
-export function SEARCH_MENU() {
+// 메뉴 조회
+export function SEARCH_MENU(MENU_NAME) {
     return request({
-        url: '/SEARCH_MENU',
-        method: 'get'
-    })
-}
+    url: '/SEARCH_MENU?data={MENU_NAME:"' + MENU_NAME + '"}',
+    method: 'get'
+    });
+  }
 
-//메뉴 목록 조회
-export function SAVE_MENU() {
+// 메뉴 저장
+export function SAVE_MENU(data) {
     return request({
         url: '/SAVE_MENU',
-        method: 'get'
+        data: data,
+        method: 'POST'
     })
 }
 
-//메뉴 목록 조회
-export function DELETE_MENU() {
+// 메뉴 삭제
+export function DELETE_MENU(data) {
     return request({
         url: '/DELETE_MENU',
-        method: 'get'
+        data: data,
+        method: 'POST'
     })
 }
