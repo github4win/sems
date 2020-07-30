281d75eeeca0dc25c33ec855c306c633da3c1cc9
github4win <52733132+github4win@users.noreply.github.com>
2020-07-30 16:50:37 +0900

    CJH 지역코드 관리 작업

--- src/api/Area_Management.js
+++ src/api/Area_Management.js
@@ -1,26 +1,26 @@
 import request from '@/utils/request'
 
 // 메뉴 조회
-export function SEARCH_MENU(MENU_NAME) {
+export function SEARCH_AREA(AREA_NAME) {
     return request({
-    url: '/SEARCH_MENU?data={MENU_NAME:"' + MENU_NAME + '"}',
+    url: '/SEARCH_Area?data={AREA_NAME:"' + AREA_NAME + '"}',
     method: 'get'
     });
   }
 
 // 메뉴 저장
-export function SAVE_MENU(data) {
+export function SAVE_AREA(data) {
     return request({
-        url: '/SAVE_MENU',
+        url: '/SAVE_Area',
         data: data,
         method: 'POST'
     })
 }
 
 // 메뉴 삭제
-export function DELETE_MENU(data) {
+export function DELETE_AREA(data) {
     return request({
-        url: '/DELETE_MENU',
+        url: '/DELETE_AREA',
         data: data,
         method: 'POST'
     })
